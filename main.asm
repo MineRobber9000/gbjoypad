9a649fccd292763514906ef277d2b2ea6405c8e8
khuxkm fbexl <khuxkm@ttm.sh>
2018-11-02 18:50:45 -0400

    Clean up RST handlers

--- main.asm
+++ main.asm
@@ -1,26 +1,13 @@
 INCLUDE "gbhw.inc"
 INCLUDE "ibmpc1.inc"
 ; rst vectors
-SECTION "rst00",ROM0[0]
+SECTION "Dummy RST Handlers",ROM0[0]
+REPT 8
 	ret
-
-SECTION "rst08",ROM0[8]
-	ret
-
-SECTION "rst10",ROM0[$10]
-	ret
-
-SECTION "rst18",ROM0[$18]
-	ret
-
-SECTION "rst20",ROM0[$20]
-	ret
-
-SECTION "rst30",ROM0[$30]
-	ret
-
-SECTION "rst38",ROM0[$38]
-	ret
+REPT 7
+	nop
+ENDR
+ENDR
 
 SECTION "vblank",ROM0[$40]
 	jp VBlank
